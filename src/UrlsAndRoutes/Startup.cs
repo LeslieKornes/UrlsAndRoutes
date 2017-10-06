0a108ce6ad195215fb65b196cb6f7c325994e725
Leslie Kornes <Lesliekornes@gmail.com>
2017-10-06 01:13:39 -0400

    initial commit

---
+++ src/UrlsAndRoutes/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UrlsAndRoutes
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
